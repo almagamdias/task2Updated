46fe07f0f151ac041dd0558045009bc81510e4f2
almagamdias <dias.almagambetov01@gmail.com>
2024-01-17 18:20:41 +0500

    H3

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,8 +1,15 @@
+import { lazy, Suspense } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-import { MainPage, ComicsPage, SingleComicPage, Page404 } from '../pages';
 import AppHeader from "../appHeader/AppHeader";
+import Spinner from '../spinner/Spinner';
 
+const Page404 = lazy(() => import('../pages/404'));
+const MainPage = lazy(() => import('../pages/MainPage'));
+const ComicsPage = lazy(() => import('../pages/ComicsPage'));
+const SingleComicPage = lazy(() => import('../pages/SingleComicPage'));
+
+// 751
 const App = () => {
 
     return (
@@ -10,16 +17,18 @@ const App = () => {
             <div className="app">
                 <AppHeader />
                 <main>
-                    <Routes>
-                        <Route path="/" element={<MainPage />}>
-                        </Route>
-                        <Route path="/comics" element={<ComicsPage />}>
-                        </Route>
-                        <Route path="/comics/:comicId" element={<SingleComicPage />}>
-                        </Route>
-                        <Route path="*" element={<Page404 />}>
-                        </Route>
-                    </Routes>
+                    <Suspense fallback={<Spinner />}>
+                        <Routes>
+                            <Route path="/" element={<MainPage />}>
+                            </Route>
+                            <Route path="/comics" element={<ComicsPage />}>
+                            </Route>
+                            <Route path="/comics/:comicId" element={<SingleComicPage />}>
+                            </Route>
+                            <Route path="*" element={<Page404 />}>
+                            </Route>
+                        </Routes>
+                    </Suspense>
                 </main>
             </div>
         </Router>
