b9e877528e83b70e3d72d1e77f00ef2c662d37d9
almagamdias <dias.almagambetov01@gmail.com>
2024-01-17 18:43:54 +0500

    H4

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -7,9 +7,9 @@ import Spinner from '../spinner/Spinner';
 const Page404 = lazy(() => import('../pages/404'));
 const MainPage = lazy(() => import('../pages/MainPage'));
 const ComicsPage = lazy(() => import('../pages/ComicsPage'));
-const SingleComicPage = lazy(() => import('../pages/SingleComicPage'));
-
-// 751
+const SingleComicLayout = lazy(() => import('../pages/singleComicLayout/SingleComicLayout'));
+const SingleCharacterLayout = lazy(() => import('../pages/singleCharacterLayout/SingleCharacterLayout'));
+const SinglePage = lazy(() => import('../pages/SinglePage'));
 const App = () => {
 
     return (
@@ -23,7 +23,9 @@ const App = () => {
                             </Route>
                             <Route path="/comics" element={<ComicsPage />}>
                             </Route>
-                            <Route path="/comics/:comicId" element={<SingleComicPage />}>
+                            <Route path="/comics/:id" element={<SinglePage Component={SingleComicLayout} dataType='comic' />}>
+                            </Route>
+                            <Route path="/characters/:id" element={<SinglePage Component={SingleCharacterLayout} dataType='character' />}>
                             </Route>
                             <Route path="*" element={<Page404 />}>
                             </Route>
